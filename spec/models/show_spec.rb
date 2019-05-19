4bd6fc22cda3db95dbdd0139dca9bac961f615fb
Ben Ricker <bgricker@bgricker.com>
2019-05-19 10:49:30 -0400

    tests for date ranges

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -2,23 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Show, type: :model do
   let(:user) { FactoryBot.create(:user) }
-  let!(:show) { FactoryBot.create(:show, user: user) }
 
   describe "#formatted_date" do
+    let!(:show) { FactoryBot.create(:show, user: user) }
+
     it "converts the date string to a Date object" do
       expect(show.formatted_date.class).to eq(Date)
     end
   end
 
   describe "#valid_shows" do
-    let!(:show) { FactoryBot.create(:show, user: user, dates: Date.current.to_s) }
-    let!(:second_show) { FactoryBot.create(:show, user: user) }
-    let!(:third_show) { FactoryBot.create(:show, user: user) }
-    let!(:past_show) { FactoryBot.create(:show, user: user, dates: "January 1 2009") }
+    context "with single dates" do
+      let!(:show) { FactoryBot.create(:show, user: user, dates: Date.current.to_s) }
+      let!(:second_show) { FactoryBot.create(:show, user: user) }
+      let!(:third_show) { FactoryBot.create(:show, user: user) }
+      let!(:past_show) { FactoryBot.create(:show, user: user, dates: "January 1 2009") }
 
-    it "shows only future dates" do
-      expect(described_class.valid_shows).to eq([show, second_show, third_show])
-      expect(described_class.valid_shows).not_to include(past_show)
+      it "shows only future dates" do
+        expect(described_class.valid_shows).to eq([show, second_show, third_show])
+        expect(described_class.valid_shows).not_to include(past_show)
+      end
+    end
+
+    context "with range dates" do
+      let!(:first_range_show) { FactoryBot.create(:show, user: user, dates: "October 25 - October 31") }
+      let!(:second_range_show) { FactoryBot.create(:show, user: user, dates: "September 5 - September 8") }
+      let!(:past_range_show) { FactoryBot.create(:show, user: user, dates: "January 25 2015 - January 29 2015") }
+
+      it "shows only future dates" do
+        expect(described_class.valid_shows).to eq([second_range_show, first_range_show])
+        expect(described_class.valid_shows).not_to include(past_range_show)
+      end
     end
   end
 end

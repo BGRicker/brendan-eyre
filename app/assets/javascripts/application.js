0b61df027bb042e137188391be55bd5d82f80c1b
Ben Ricker <bgricker@bgricker.com>
2019-06-23 20:02:40 -0400

    reordering javascript requires

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,9 @@
 //= require rails-ujs
 //= require activestorage
 //= require jquery
-//= require bootstrap-sprockets
+//= require jquery_ujs
 //= require bootstrap
+//= require bootstrap-sprockets
 //= require_tree .
 
 

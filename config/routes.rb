b566cab7a1723538b1b28d227be6e12814fb9ada
Ben Ricker <bgricker@bgricker.com>
2019-06-24 21:39:43 -0400

    simple sign in route, add show index to hamburger

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :shows
-  devise_for :users
   root 'static_pages#index'
+
+  devise_for :users
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
+
+  resources :shows
 end

cf7cf609ced88434645f486f76f1dda08a3d9115
Ben Ricker <bgricker@bgricker.com>
2025-04-23 22:39:44 -0400

    update rails, image spacing

--- config/application.rb
+++ config/application.rb
@@ -33,8 +33,7 @@ module Brendaneyre
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = 'UTC'
-    config.active_record.default_timezone = :utc
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.

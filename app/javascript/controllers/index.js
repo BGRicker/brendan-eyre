6aa8aaa43ba6dd586982bbeea90a2d177f37fd86
Ben Ricker <bgricker@bgricker.com>
2025-04-28 12:46:09 -0400

    redefine stimulus js controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,7 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start()
+const context = require.context(".", true, /\.js$/)
+window.Stimulus.load(definitionsFromContext(context))
